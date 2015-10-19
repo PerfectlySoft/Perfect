8c855c2d6029b90e4896bb9372540982a1542d55
Kyle Jessup <badthing@gmail.com>
2015-10-19 15:17:41 -0400

    PerfectLib iOS compilation
    Former-commit-id: 69a2173d18c63a8993eb0e091f545d85fa6a8783

---
+++ PerfectLib/iOS/PerfectLib.h
@@ -0,0 +1,19 @@
+//
+//  PerfectLib.h
+//  PerfectLib
+//
+//  Created by Kyle Jessup on 2015-08-11.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PerfectiOS.
+FOUNDATION_EXPORT double PerfectiOSVersionNumber;
+
+//! Project version string for PerfectiOS.
+FOUNDATION_EXPORT const unsigned char PerfectiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PerfectiOS/PublicHeader.h>
+
+
