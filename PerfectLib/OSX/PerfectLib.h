bd4600694d61d43cabbf84d9dde60b995978817a
Kyle Jessup <badthing@gmail.com>
2015-10-19 13:41:50 -0400

    PerfectLib OSX compilation

---
+++ PerfectLib/OSX/PerfectLib.h
@@ -0,0 +1,19 @@
+//
+//  PerfectLib.h
+//  PerfectLib
+//
+//  Created by Kyle Jessup on 2015-10-19.
+//  Copyright © 2015 PerfectlySoft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PerfectLib.
+FOUNDATION_EXPORT double PerfectLibVersionNumber;
+
+//! Project version string for PerfectLib.
+FOUNDATION_EXPORT const unsigned char PerfectLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PerfectLib/PublicHeader.h>
+
+
