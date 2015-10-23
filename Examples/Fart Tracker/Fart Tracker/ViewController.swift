de0712bdb4a5474513bd21ed492b3cbc88df0da3
Kyle Jessup <badthing@gmail.com>
2015-10-23 12:26:20 -0400

    Example starter

---
+++ Examples/Fart Tracker/Fart Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fart Tracker
+//
+//  Created by Kyle Jessup on 2015-10-22.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
