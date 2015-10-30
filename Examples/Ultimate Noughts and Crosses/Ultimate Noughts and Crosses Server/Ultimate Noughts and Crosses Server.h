351467712311a387fbbb411bb2cb95b985ee9a0c
Kyle Jessup <badthing@gmail.com>
2015-10-30 13:23:33 -0400

    Ensuring everything is committed.

---
+++ Examples/Ultimate Noughts and Crosses/Ultimate Noughts and Crosses Server/Ultimate Noughts and Crosses Server.h
@@ -0,0 +1,19 @@
+//
+//  Ultimate Noughts and Crosses Server.h
+//  Ultimate Noughts and Crosses Server
+//
+//  Created by Kyle Jessup on 2015-10-28.
+//  Copyright © 2015 PerfectlySoft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Ultimate Noughts and Crosses Server.
+FOUNDATION_EXPORT double Ultimate_Noughts_and_Crosses_ServerVersionNumber;
+
+//! Project version string for Ultimate Noughts and Crosses Server.
+FOUNDATION_EXPORT const unsigned char Ultimate_Noughts_and_Crosses_ServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ultimate_Noughts_and_Crosses_Server/PublicHeader.h>
+
+
