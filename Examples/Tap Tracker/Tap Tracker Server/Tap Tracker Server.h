fd17feadef76134e2de5c441818a1446f7c9f3a8
Kyle Jessup <badthing@gmail.com>
2015-10-30 13:53:40 -0400

    Renamed example project
    Former-commit-id: ca08fd5b6c6234154dcbf43a5fd01915f511cf5a

---
+++ Examples/Tap Tracker/Tap Tracker Server/Tap Tracker Server.h
@@ -0,0 +1,19 @@
+//
+//  Fart Tracker Server.h
+//  Fart Tracker Server
+//
+//  Created by Kyle Jessup on 2015-10-22.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Fart Tracker Server.
+FOUNDATION_EXPORT double Fart_Tracker_ServerVersionNumber;
+
+//! Project version string for Fart Tracker Server.
+FOUNDATION_EXPORT const unsigned char Fart_Tracker_ServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fart_Tracker_Server/PublicHeader.h>
+
+
