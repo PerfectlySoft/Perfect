af5a6e7aa18214a923ee5fb928ab7b42e2f6f65d
Kyle Jessup <badthing@gmail.com>
2015-11-05 15:08:01 -0500

    Started Upload Enumerator example project
    Former-commit-id: 76f9a165466591ec765ce85d184893c72d8783e0

---
+++ Examples/Upload Enumerator/Upload Enumerator/Upload Enumerator.h
@@ -0,0 +1,19 @@
+//
+//  Upload Enumerator.h
+//  Upload Enumerator
+//
+//  Created by Kyle Jessup on 2015-11-05.
+//  Copyright © 2015 PerfectlySoft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Upload Enumerator.
+FOUNDATION_EXPORT double Upload_EnumeratorVersionNumber;
+
+//! Project version string for Upload Enumerator.
+FOUNDATION_EXPORT const unsigned char Upload_EnumeratorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Upload_Enumerator/PublicHeader.h>
+
+
