baebc292df7c1a20aa0861b82bd18d5d7fddf672
Kyle Jessup <badthing@gmail.com>
2015-11-09 16:23:06 -0500

    Updated license and copyright information
    Former-commit-id: eae519de88222f46ed0223b6331dc88f06deb668

--- Examples/Tap Tracker/Tap Tracker/MapViewController.swift
+++ Examples/Tap Tracker/Tap Tracker/MapViewController.swift
@@ -3,8 +3,22 @@
 //  Tap Tracker
 //
 //  Created by Kyle Jessup on 2015-10-27.
+//	Copyright (C) 2015 PerfectlySoft, Inc.
 //
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU Affero General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
 //
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU Affero General Public License for more details.
+//
+//     You should have received a copy of the GNU Affero General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
 
 import UIKit
 import MapKit
