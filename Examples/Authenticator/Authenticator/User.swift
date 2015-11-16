15152ea594f7b95ae18d9212f74a45dd2d9ad740
Kyle Jessup <badthing@gmail.com>
2015-11-16 09:54:55 -0500

    Put in new license and copyright notices
    Former-commit-id: 7cdc6220fa975da16df76b1934c238624c494055

--- Examples/Authenticator/Authenticator/User.swift
+++ Examples/Authenticator/Authenticator/User.swift
@@ -5,18 +5,22 @@
 //  Created by Kyle Jessup on 2015-11-09.
 //	Copyright (C) 2015 PerfectlySoft, Inc.
 //
-//     This program is free software: you can redistribute it and/or modify
-//     it under the terms of the GNU Affero General Public License as published by
-//     the Free Software Foundation, either version 3 of the License, or
-//     (at your option) any later version.
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU Affero General Public License as
+//	published by the Free Software Foundation, either version 3 of the
+//	License, or (at your option) any later version, as supplemented by the
+//	Perfect Additional Terms.
 //
-//     This program is distributed in the hope that it will be useful,
-//     but WITHOUT ANY WARRANTY; without even the implied warranty of
-//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//     GNU Affero General Public License for more details.
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU Affero General Public License, as supplemented by the
+//	Perfect Additional Terms, for more details.
 //
-//     You should have received a copy of the GNU Affero General Public License
-//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//	You should have received a copy of the GNU Affero General Public License
+//	and the Perfect Additional Terms that immediately follow the terms and
+//	conditions of the GNU Affero General Public License along with this
+//	program. If not, see <http://www.perfect.org/AGPL_3_0_With_Perfect_Additional_Terms.txt>.
 //
 
 
