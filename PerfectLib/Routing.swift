4853586adc38053630d98416f5107ad1c1f650a4
Kyle Jessup <badthing@gmail.com>
2015-12-16 14:39:46 -0500

    A bit of code reorg.

--- PerfectLib/Routing.swift
+++ PerfectLib/Routing.swift
@@ -25,6 +25,152 @@
 
 public typealias RequestHandlerGenerator = PageHandlerRegistry.RequestHandlerGenerator
 
+/// Holds the registered routes.
+public struct RouteMap: CustomStringConvertible {
+	
+	/// Pretty prints all route information.
+	public var description: String {
+		var s = self.root.description
+		for (method, root) in self.methodRoots {
+			s.appendContentsOf("\n" + method + ":\n" + root.description)
+		}
+		return s
+	}
+	
+	private var root = RouteNode() // root node for any request method
+	private var methodRoots = [String:RouteNode]() // by convention, use all upper cased method names for inserts/lookups
+	
+	/// Lookup a route based on the URL path.
+	/// Returns the handler generator if found.
+	subscript(path: String, webResponse: WebResponse) -> RequestHandlerGenerator? {
+		get {
+			let components = path.lowercaseString.pathComponents
+			var g = components.generate()
+			let _ = g.next() // "/"
+			
+			let method = webResponse.request.requestMethod().uppercaseString
+			if let root = self.methodRoots[method] {
+				if let handler = root.findHandler("", generator: g, webResponse: webResponse) {
+					return handler
+				}
+			}
+			return self.root.findHandler("", generator: g, webResponse: webResponse)
+		}
+	}
+	
+	/// Add a route to the system.
+	/// `Routing.Routes["/foo/*/baz"] = { _ in return ExampleHandler() }`
+	public subscript(path: String) -> RequestHandlerGenerator? {
+		get {
+			return nil // Swift does not currently allow set-only subscripts
+		}
+		set {
+			self.root.addPathSegments(path.lowercaseString.pathComponents.generate(), h: newValue!)
+		}
+	}
+	
+	/// Add an array of routes for a given handler.
+	/// `Routing.Routes[ ["/", "index.html"] ] = { _ in return ExampleHandler() }`
+	public subscript(paths: [String]) -> RequestHandlerGenerator? {
+		get {
+			return nil
+		}
+		set {
+			for path in paths {
+				self[path] = newValue
+			}
+		}
+	}
+	
+	/// Add a route to the system using the indicated HTTP request method.
+	/// `Routing.Routes["GET", "/foo/*/baz"] = { _ in return ExampleHandler() }`
+	public subscript(method: String, path: String) -> RequestHandlerGenerator? {
+		get {
+			return nil // Swift does not currently allow set-only subscripts
+		}
+		set {
+			let uppered = method.uppercaseString
+			if let root = self.methodRoots[uppered] {
+				root.addPathSegments(path.lowercaseString.pathComponents.generate(), h: newValue!)
+			} else {
+				let root = RouteNode()
+				self.methodRoots[uppered] = root
+				root.addPathSegments(path.lowercaseString.pathComponents.generate(), h: newValue!)
+			}
+		}
+	}
+	
+	/// Add an array of routes for a given handler using the indicated HTTP request method.
+	/// `Routing.Routes["GET", ["/", "index.html"] ] = { _ in return ExampleHandler() }`
+	public subscript(method: String, paths: [String]) -> RequestHandlerGenerator? {
+		get {
+			return nil // Swift does not currently allow set-only subscripts
+		}
+		set {
+			for path in paths {
+				self[method, path] = newValue
+			}
+		}
+	}
+}
+
+/// This wraps up the routing related functionality.
+/// Enable the routing system by calling:
+/// ```
+/// Routing.Handler.registerGlobally()
+/// ```
+/// This should be done in your `PerfectServerModuleInit` function.
+/// The system supports HTTP method based routing, wildcards and variables.
+///
+/// Add routes in the following manner:
+/// ```
+/// 	Routing.Routes["GET", ["/", "index.html"] ] = { (_:WebResponse) in return IndexHandler() }
+/// 	Routing.Routes["/foo/*/baz"] = { _ in return EchoHandler() }
+/// 	Routing.Routes["/foo/bar/baz"] = { _ in return EchoHandler() }
+/// 	Routing.Routes["GET", "/user/{id}/baz"] = { _ in return Echo2Handler() }
+/// 	Routing.Routes["POST", "/user/{id}/baz"] = { _ in return Echo3Handler() }
+/// ```
+/// The closure you provide should return an instance of `PageHandler`. It is provided the WebResponse object to permit further customization.
+/// Variables set by the routing process can be accessed through the `WebRequest.urlVariables` dictionary.
+/// Note that a PageHandler *MUST* call `WebResponse.requestCompletedCallback()` when the request has completed.
+/// This does not need to be done within the `handleRequest` method.
+public class Routing {
+	
+	/// The routes which have been configured.
+	static public var Routes = RouteMap()
+	
+	private init() {}
+	
+	/// This is the main handler for the logic of the URL routing system.
+	/// If must be enabled by calling `Routing.Handler.registerGlobally`
+	public class Handler: RequestHandler {
+		
+		/// Install the URL routing system.
+		/// This is required if this system is to be utilized, otherwise it will not be available.
+		static public func registerGlobally() {
+			PageHandlerRegistry.addRequestHandler { (_:WebResponse) -> RequestHandler in
+				return Routing.Handler()
+			}
+		}
+		
+		/// Handle the request, triggering the routing system.
+		/// If a route is discovered the request is sent to the new handler.
+		public func handleRequest(request: WebRequest, response: WebResponse) {
+			let pathInfo = request.pathInfo()
+			
+			if let handler = Routing.Routes[pathInfo, response] {
+				handler(response).handleRequest(request, response: response)
+			} else {
+				response.setStatus(404, message: "NOT FOUND")
+				response.appendBodyString("The file \(pathInfo) was not found.")
+				response.requestCompletedCallback()
+			}
+		}
+		
+	}
+	
+}
+
 class RouteNode: CustomStringConvertible {
 	
 	typealias ComponentGenerator = IndexingGenerator<[String]>
@@ -213,167 +359,6 @@ class RouteVariable: RouteNode {
 	
 }
 
-class RequestMethodFilter: RouteVariable {
-	
-	override func descriptionTabbed(tabCount: Int) -> String {
-		var s = "/[\(self.name)]"
-		
-		if let _ = self.handlerGenerator {
-			s.appendContentsOf("+h\n")
-		} else {
-			s.appendContentsOf("\n")
-		}
-		s.appendContentsOf(self.descriptionTabbedInner(tabCount))
-		return s
-	}
-	
-}
-
-/// Holds the registered routes.
-public struct RouteMap: CustomStringConvertible {
-	
-	/// Pretty prints all route information.
-	public var description: String {
-		var s = self.root.description
-		for (method, root) in self.methodRoots {
-			s.appendContentsOf("\n" + method + ":\n" + root.description)
-		}
-		return s
-	}
-	
-	private var root = RouteNode() // root node for any request method
-	private var methodRoots = [String:RouteNode]() // by convention, use all upper cased method names for inserts/lookups
-	
-	/// Lookup a route based on the URL path.
-	/// Returns the handler generator if found.
-	subscript(path: String, webResponse: WebResponse) -> RequestHandlerGenerator? {
-		get {
-			let components = path.lowercaseString.pathComponents
-			var g = components.generate()
-			let _ = g.next() // "/"
-			
-			let method = webResponse.request.requestMethod().uppercaseString
-			if let root = self.methodRoots[method] {
-				if let handler = root.findHandler("", generator: g, webResponse: webResponse) {
-					return handler
-				}
-			}
-			return self.root.findHandler("", generator: g, webResponse: webResponse)
-		}
-	}
-	
-	/// Add a route to the system.
-	/// `Routing.Routes["/foo/*/baz"] = { _ in return ExampleHandler() }`
-	public subscript(path: String) -> RequestHandlerGenerator? {
-		get {
-			return nil // Swift does not currently allow set-only subscripts
-		}
-		set {
-			self.root.addPathSegments(path.lowercaseString.pathComponents.generate(), h: newValue!)
-		}
-	}
-	
-	/// Add an array of routes for a given handler.
-	/// `Routing.Routes[ ["/", "index.html"] ] = { _ in return ExampleHandler() }`
-	public subscript(paths: [String]) -> RequestHandlerGenerator? {
-		get {
-			return nil
-		}
-		set {
-			for path in paths {
-				self[path] = newValue
-			}
-		}
-	}
-	
-	/// Add a route to the system using the indicated HTTP request method.
-	/// `Routing.Routes["GET", "/foo/*/baz"] = { _ in return ExampleHandler() }`
-	public subscript(method: String, path: String) -> RequestHandlerGenerator? {
-		get {
-			return nil // Swift does not currently allow set-only subscripts
-		}
-		set {
-			let uppered = method.uppercaseString
-			if let root = self.methodRoots[uppered] {
-				root.addPathSegments(path.lowercaseString.pathComponents.generate(), h: newValue!)
-			} else {
-				let root = RouteNode()
-				self.methodRoots[uppered] = root
-				root.addPathSegments(path.lowercaseString.pathComponents.generate(), h: newValue!)
-			}
-		}
-	}
-	
-	/// Add an array of routes for a given handler using the indicated HTTP request method.
-	/// `Routing.Routes["GET", ["/", "index.html"] ] = { _ in return ExampleHandler() }`
-	public subscript(method: String, paths: [String]) -> RequestHandlerGenerator? {
-		get {
-			return nil // Swift does not currently allow set-only subscripts
-		}
-		set {
-			for path in paths {
-				self[method, path] = newValue
-			}
-		}
-	}
-}
-
-/// This wraps up the routing related functionality.
-/// Enable the routing system by calling:
-/// ```
-/// Routing.Handler.registerGlobally()
-/// ```
-/// This should be done in your `PerfectServerModuleInit` function.
-/// The system supports HTTP method based routing, wildcards and variables.
-///
-/// Add routes in the following manner:
-/// ```
-/// 	Routing.Routes["GET", ["/", "index.html"] ] = { (_:WebResponse) in return IndexHandler() }
-/// 	Routing.Routes["/foo/*/baz"] = { _ in return EchoHandler() }
-/// 	Routing.Routes["/foo/bar/baz"] = { _ in return EchoHandler() }
-/// 	Routing.Routes["GET", "/user/{id}/baz"] = { _ in return Echo2Handler() }
-/// 	Routing.Routes["POST", "/user/{id}/baz"] = { _ in return Echo3Handler() }
-/// ```
-/// The closure you provide should return an instance of `PageHandler`. It is provided the WebResponse object to permit further customization.
-/// Variables set by the routing process can be accessed through the `WebRequest.urlVariables` dictionary.
-/// Note that a PageHandler *MUST* call `WebResponse.requestCompletedCallback()` when the request has completed.
-/// This does not need to be done within the `handleRequest` method.
-public class Routing {
-	
-	/// The routes which have been configured.
-	static public var Routes = RouteMap()
-	
-	private init() {}
-	
-	/// This is the main handler for the logic of the URL routing system.
-	/// If must be enabled by calling `Routing.Handler.registerGlobally`
-	public class Handler: RequestHandler {
-		
-		/// Install the URL routing system.
-		/// This is required if this system is to be utilized, otherwise it will not be available.
-		static public func registerGlobally() {
-			PageHandlerRegistry.addRequestHandler { (_:WebResponse) -> RequestHandler in
-				return Routing.Handler()
-			}
-		}
-		
-		/// Handle the request, triggering the routing system.
-		/// If a route is discovered the request is sent to the new handler.
-		public func handleRequest(request: WebRequest, response: WebResponse) {
-			let pathInfo = request.pathInfo()
-			
-			if let handler = Routing.Routes[pathInfo, response] {
-				handler(response).handleRequest(request, response: response)
-			} else {
-				response.setStatus(404, message: "NOT FOUND")
-				response.appendBodyString("The file \(pathInfo) was not found.")
-				response.requestCompletedCallback()
-			}
-		}
-		
-	}
-	
-}
 
 
 
