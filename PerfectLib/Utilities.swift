4b1620a2a4225f4eeca7aed0c5c8cf2ef62d0aa7
Takeo Namba <nanba@groovelab.asia>
2016-01-15 23:51:45 +0900

    internal to public

--- PerfectLib/Utilities.swift
+++ PerfectLib/Utilities.swift
@@ -279,7 +279,7 @@ extension String {
 
 extension String {
 	
-	func stringByReplacingString(find: String, withString: String) -> String {
+	public func stringByReplacingString(find: String, withString: String) -> String {
 		
 		guard !find.isEmpty else {
 			return self
@@ -316,7 +316,7 @@ extension String {
 		return ret
 	}
 	
-	func substringTo(index: String.Index) -> String {
+	public func substringTo(index: String.Index) -> String {
 		var s = ""
 		var idx = self.startIndex
 		let endIdx = self.endIndex
@@ -327,7 +327,7 @@ extension String {
 		return s
 	}
 	
-	func substringWith(range: Range<String.Index>) -> String {
+	public func substringWith(range: Range<String.Index>) -> String {
 		var s = ""
 		var idx = range.startIndex
 		let endIdx = self.endIndex
