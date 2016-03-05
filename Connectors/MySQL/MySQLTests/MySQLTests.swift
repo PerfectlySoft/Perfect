3d41dcdb70f5a43de93286b268380b15b535dbbc
Takeo Namba <nanba@groovelab.asia>
2016-03-05 12:41:37 +0900

    use mysql in "set up"
    Former-commit-id: 44cabc6a7f0b1095b83d2fd0266e10ef64ceab13

--- Connectors/MySQL/MySQLTests/MySQLTests.swift
+++ Connectors/MySQL/MySQLTests/MySQLTests.swift
@@ -40,9 +40,11 @@ class MySQLTests: XCTestCase {
 
         //  connect and select DB
         mysql = MySQL()
+        XCTAssert(mysql.setOption(.MYSQL_SET_CHARSET_NAME, "utf8mb4"), mysql.errorMessage())
         XCTAssert(mysql.connect(HOST, user: USER, password: PASSWORD), mysql.errorMessage())
         if mysql.selectDatabase(SCHEMA) == false {
             XCTAssert(mysql.query("CREATE SCHEMA `\(SCHEMA)` DEFAULT CHARACTER SET utf8mb4"), mysql.errorMessage())
+            XCTAssert(mysql.selectDatabase(SCHEMA), mysql.errorMessage())
         }
     }
     
@@ -87,56 +89,22 @@ class MySQLTests: XCTestCase {
 	}
 	
 	func testListDbs1() {
-		
-		let mysql = MySQL()
-		let res = mysql.connect(HOST, user: USER, password: PASSWORD)
-		
-		XCTAssert(res)
-		
-		if !res {
-			print(mysql.errorMessage())
-		}
-		
 		let list = mysql.listDatabases()
 		
 		XCTAssert(list.count > 0)
 		
 		print(list)
-		
-		mysql.close()
 	}
 	
 	func testListDbs2() {
-		
-		let mysql = MySQL()
-		let res = mysql.connect(HOST, user: USER, password: PASSWORD)
-		
-		XCTAssert(res)
-		
-		if !res {
-			print(mysql.errorMessage())
-		}
-		
 		let list = mysql.listDatabases("information_%")
 		
 		XCTAssert(list.count > 0)
 		
 		print(list)
-		
-		mysql.close()
 	}
 	
 	func testListTables1() {
-		
-		let mysql = MySQL()
-		let res = mysql.connect(HOST, user: USER, password: PASSWORD)
-		
-		XCTAssert(res)
-		
-		if !res {
-			print(mysql.errorMessage())
-		}
-		
 		let sres = mysql.selectDatabase("information_schema")
 		
 		XCTAssert(sres == true)
@@ -146,21 +114,9 @@ class MySQLTests: XCTestCase {
 		XCTAssert(list.count > 0)
 		
 		print(list)
-		
-		mysql.close()
 	}
 	
 	func testListTables2() {
-		
-		let mysql = MySQL()
-		let res = mysql.connect(HOST, user: USER, password: PASSWORD)
-		
-		XCTAssert(res)
-		
-		if !res {
-			print(mysql.errorMessage())
-		}
-		
 		let sres = mysql.selectDatabase("information_schema")
 		
 		XCTAssert(sres == true)
@@ -170,27 +126,11 @@ class MySQLTests: XCTestCase {
 		XCTAssert(list.count > 0)
 		
 		print(list)
-		
-		mysql.close()
 	}
 	
 	func testQuery1() {
-		let mysql = MySQL()
-		defer {
-			mysql.close()
-		}
-		
-		let res = mysql.connect(HOST, user: USER, password: PASSWORD)
-		XCTAssert(res)
-		
-		if !res {
-			print(mysql.errorMessage())
-			return
-		}
-		
-		let sres = mysql.selectDatabase(SCHEMA)
-		XCTAssert(sres == true)
-		
+        mysql.query("DROP TABLE IF EXISTS test")
+
 		let qres = mysql.query("CREATE TABLE test (id INT, d DOUBLE, s VARCHAR(1024))")
 		XCTAssert(qres == true, mysql.errorMessage())
 		
@@ -222,21 +162,8 @@ class MySQLTests: XCTestCase {
 	}
 	
 	func testQuery2() {
-		let mysql = MySQL()
-		defer {
-			mysql.close()
-		}
-		
-		let res = mysql.connect(HOST, user: USER, password: PASSWORD)
-		XCTAssert(res)
-		
-		if !res {
-			print(mysql.errorMessage())
-		}
-		
-		let sres = mysql.selectDatabase(SCHEMA)
-		XCTAssert(sres == true)
-		
+        mysql.query("DROP TABLE IF EXISTS test")
+
 		let qres = mysql.query("CREATE TABLE test (id INT, d DOUBLE, s VARCHAR(1024))")
 		XCTAssert(qres == true, mysql.errorMessage())
 		
@@ -268,30 +195,13 @@ class MySQLTests: XCTestCase {
 	}
 	
 	func testQueryStmt1() {
-		let mysql = MySQL()
-		defer {
-			mysql.close()
-		}
-		
-		let res = mysql.connect(HOST, user: USER, password: PASSWORD)
-		XCTAssert(res)
-		
-		if !res {
-			print(mysql.errorMessage())
-		}
-		
-		let sres = mysql.selectDatabase(SCHEMA)
-		XCTAssert(sres == true)
-		
 		mysql.query("DROP TABLE IF EXISTS all_data_types")
 		
 		let qres = mysql.query("CREATE TABLE `all_data_types` (`varchar` VARCHAR( 20 ),\n`tinyint` TINYINT,\n`text` TEXT,\n`date` DATE,\n`smallint` SMALLINT,\n`mediumint` MEDIUMINT,\n`int` INT,\n`bigint` BIGINT,\n`float` FLOAT( 10, 2 ),\n`double` DOUBLE,\n`decimal` DECIMAL( 10, 2 ),\n`datetime` DATETIME,\n`timestamp` TIMESTAMP,\n`time` TIME,\n`year` YEAR,\n`char` CHAR( 10 ),\n`tinyblob` TINYBLOB,\n`tinytext` TINYTEXT,\n`blob` BLOB,\n`mediumblob` MEDIUMBLOB,\n`mediumtext` MEDIUMTEXT,\n`longblob` LONGBLOB,\n`longtext` LONGTEXT,\n`enum` ENUM( '1', '2', '3' ),\n`set` SET( '1', '2', '3' ),\n`bool` BOOL,\n`binary` BINARY( 20 ),\n`varbinary` VARBINARY( 20 ) ) ENGINE = MYISAM")
 		XCTAssert(qres == true, mysql.errorMessage())
 		
 		let stmt1 = MySQLStmt(mysql)
-		defer {
-			stmt1.close()
-		}
+		defer { stmt1.close() }
 		let prepRes = stmt1.prepare("INSERT INTO all_data_types VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)")
 		XCTAssert(prepRes, stmt1.errorMessage())
 		XCTAssert(stmt1.paramCount() == 28)
@@ -334,28 +244,10 @@ class MySQLTests: XCTestCase {
 			
 			let execRes = stmt1.execute()
 			XCTAssert(execRes, "\(stmt1.errorCode()) \(stmt1.errorMessage()) - \(mysql.errorCode()) \(mysql.errorMessage())")
-			
-			stmt1.close()
 		}
 	}
 	
 	func testQueryStmt2() {
-		let mysql = MySQL()
-		defer {
-			mysql.close()
-		}
-		
-		mysql.setOption(.MYSQL_SET_CHARSET_NAME, "utf8mb4")
-		let res = mysql.connect(HOST, user: USER, password: PASSWORD)
-		XCTAssert(res)
-		
-		if !res {
-			print(mysql.errorMessage())
-		}
-		
-		let sres = mysql.selectDatabase(SCHEMA)
-		XCTAssert(sres == true)
-		
 		mysql.query("DROP TABLE IF EXISTS all_data_types")
 		
 		let qres = mysql.query("CREATE TABLE `all_data_types` (`varchar` VARCHAR( 20 ),\n`tinyint` TINYINT,\n`text` TEXT,\n`date` DATE,\n`smallint` SMALLINT,\n`mediumint` MEDIUMINT,\n`int` INT,\n`bigint` BIGINT,\n`ubigint` BIGINT UNSIGNED,\n`float` FLOAT( 10, 2 ),\n`double` DOUBLE,\n`decimal` DECIMAL( 10, 2 ),\n`datetime` DATETIME,\n`timestamp` TIMESTAMP,\n`time` TIME,\n`year` YEAR,\n`char` CHAR( 10 ),\n`tinyblob` TINYBLOB,\n`tinytext` TINYTEXT,\n`blob` BLOB,\n`mediumblob` MEDIUMBLOB,\n`mediumtext` MEDIUMTEXT,\n`longblob` LONGBLOB,\n`longtext` LONGTEXT,\n`enum` ENUM( '1', '2', '3' ),\n`set` SET( '1', '2', '3' ),\n`bool` BOOL,\n`binary` BINARY( 20 ),\n`varbinary` VARBINARY( 20 ) ) ENGINE = MYISAM")
@@ -363,9 +255,7 @@ class MySQLTests: XCTestCase {
 		
 		for _ in 1...2 {
 			let stmt1 = MySQLStmt(mysql)
-			defer {
-				stmt1.close()
-			}
+            defer { stmt1.close() }
 			let prepRes = stmt1.prepare("INSERT INTO all_data_types VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)")
 			XCTAssert(prepRes, stmt1.errorMessage())
 			XCTAssert(stmt1.paramCount() == 29)
@@ -404,13 +294,12 @@ class MySQLTests: XCTestCase {
 				
 				let execRes = stmt1.execute()
 				XCTAssert(execRes, "\(stmt1.errorCode()) \(stmt1.errorMessage()) - \(mysql.errorCode()) \(mysql.errorMessage())")
-				
-				stmt1.close()
 			}
 		}
 		
 		do {
 			let stmt1 = MySQLStmt(mysql)
+            defer { stmt1.close() }
 			
 			let prepRes = stmt1.prepare("SELECT * FROM all_data_types")
 			XCTAssert(prepRes, stmt1.errorMessage())
@@ -419,6 +308,7 @@ class MySQLTests: XCTestCase {
 			XCTAssert(execRes, stmt1.errorMessage())
 			
 			let results = stmt1.results()
+            defer { results.close() }
 			
 			let ok = results.forEachRow {
 				e in
@@ -457,24 +347,10 @@ class MySQLTests: XCTestCase {
                 XCTAssertEqual(e[28] as? String, "1")
 			}
 			XCTAssert(ok, stmt1.errorMessage())
-			
-			results.close()
-			stmt1.close()
 		}
 	}
 	
 	func testServerVersion() {
-		let mysql = MySQL()
-		defer {
-			mysql.close()
-		}
-		let res = mysql.connect(HOST, user: USER, password: PASSWORD)
-		XCTAssert(res)
-		
-		if !res {
-			print(mysql.errorMessage())
-		}
-		
 		let vers = mysql.serverVersion()
 		XCTAssert(vers >= 50627) // YMMV
 	}
@@ -771,163 +647,4 @@ class MySQLTests: XCTestCase {
             XCTAssertEqual(row[5] as? String, "-3.3")
         }
     }
-
-    /*
-    try dbManager.query("CREATE TABLE IF NOT EXISTS `all_data_types` (`char` CHAR( 10 ),`varchar` VARCHAR( 20 ),`tinytext` TINYTEXT,`mediumtext` MEDIUMTEXT,`text` TEXT,`longtext` LONGTEXT,"
-    + "`tinyint` TINYINT,`utinyint` TINYINT UNSIGNED,`smallint` SMALLINT,`usmallint` SMALLINT UNSIGNED,`mediumint` MEDIUMINT,`umediumint` MEDIUMINT UNSIGNED,"
-    + "`int` INT,`uint` INT UNSIGNED,`bigint` BIGINT,`ubigint` BIGINT UNSIGNED,"
-    + "`float` FLOAT( 10, 2 ),`double` DOUBLE,`decimal` DECIMAL( 10, 2 ),"
-    + "`date` DATE,`datetime` DATETIME,`timestamp` TIMESTAMP,`time` TIME,`year` YEAR,"
-    + "`tinyblob` TINYBLOB,`mediumblob` MEDIUMBLOB,`blob` BLOB,`longblob` LONGBLOB,"
-    + "`enum` ENUM( '1', '2', '3' ),`set` SET( '1', '2', '3' ),`bool` BOOL,"
-    + "`binary` BINARY( 20 ),`varbinary` VARBINARY( 20 ) )")
-    
-    try dbManager.query("DELETE FROM all_data_types")
-    
-    try dbManager.query("INSERT INTO all_data_types (`char`,`varchar`,`tinytext`,`mediumtext`,`text`,`longtext`,"
-    + "`tinyint`,`utinyint`,`smallint`,`usmallint`,`mediumint`,`umediumint`,"
-    + "`int`,`uint`,`bigint`,`ubigint`,"
-    + "`float`, `double`, `decimal`,"
-    + "`date`, `datetime`, `timestamp`, `time`,`year`,"
-    + "`tinyblob`, `mediumblob`, `blob`,`longblob`,"
-    + "`enum`,`set`,`bool`,"
-    + "`binary`,`varbinary` ) VALUES ("
-    + "'a','abc','tiny text','medium text','text','long text',"
-    + "-1, 1, -2, 2, -3, 3,"
-    + "-4, 4, -5, 5,"
-    + "1.1, 2.2, 123,"
-    + "'2015-10-21','2015-10-21 11:22:33','2015-10-21 11:22:33','11:22:33','2016',"
-    + "'abc','abc','abc','abc',"
-    + "'1','2',true,"
-    + "'1','2')")
-    try dbManager.query("SELECT * FROM all_data_types LIMIT 1")
-    let results = try dbManager.storeResults()
-    defer { results.close() }
-    while let row = results.next() {
-    print(row)
-    }
-    
-    try dbManager.query("DELETE FROM all_data_types")
-    
-    
-    let stmt1 = MySQLStmt(dbManager.db)
-    defer { stmt1.close() }
-    let prepRes = stmt1.prepare("INSERT INTO all_data_types VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)")
-    //        print(prepRes)
-    stmt1.bindParam("a")
-    stmt1.bindParam("abc")
-    stmt1.bindParam("tiny text")
-    stmt1.bindParam("medium text")
-    stmt1.bindParam("text")
-    stmt1.bindParam("long text")
-    stmt1.bindParam(-1)
-    stmt1.bindParam(1)
-    stmt1.bindParam(-2)
-    stmt1.bindParam(2)
-    stmt1.bindParam(-3)
-    stmt1.bindParam(3)
-    stmt1.bindParam(-4)
-    stmt1.bindParam(4)
-    stmt1.bindParam(-5)
-    stmt1.bindParam(5)
-    stmt1.bindParam(1.1)
-    stmt1.bindParam(2.2)
-    stmt1.bindParam(123)
-    stmt1.bindParam("2015-10-21")
-    stmt1.bindParam("2015-10-21 11:22:33")
-    stmt1.bindParam("2015-10-21 11:22:33")
-    stmt1.bindParam("11:22:33")
-    stmt1.bindParam("2016")
-    
-    stmt1.bindParam("tinyblob👻")
-    stmt1.bindParam("mediumblob")
-    stmt1.bindParam("blob")
-    stmt1.bindParam("longblob")
-    //        "tinyblob".withCString { stmt1.bindParam($0, length: 8) }
-    //        "mediumblob".withCString { stmt1.bindParam($0, length: 10) }
-    //        "blob".withCString { stmt1.bindParam($0, length: 4) }
-    //        "longblob".withCString { stmt1.bindParam($0, length: 8) }
-    stmt1.bindParam("1")
-    stmt1.bindParam("2")
-    stmt1.bindParam(1)
-    stmt1.bindParam("1")
-    stmt1.bindParam("2")
-    let execRes = stmt1.execute()
-    //        print(execRes)
-    let stmt2 = MySQLStmt(dbManager.db)
-    defer { stmt2.close() }
-    //        let prepRes2 = stmt2.prepare("SELECT `text`, 'blob' FROM all_data_types")
-    let prepRes2 = stmt2.prepare("SELECT * FROM all_data_types")
-    //        print(prepRes2)
-    let execRes2 = stmt2.execute()
-    let results2 = stmt2.results()
-    defer { results2.close() }
-    results2.forEachRow { row in
-    //            print("text", UTF8Encoding.encode(row[0] as! [UInt8]))
-    //            print("blob", row[1] as! String)
-    
-    print("char", row[0] as! String)
-    print("varchar", row[1] as! String)
-    print("tinytext", UTF8Encoding.encode(row[2] as! [UInt8]))
-    print("mediumtext", UTF8Encoding.encode(row[3] as! [UInt8]))
-    print("text", UTF8Encoding.encode(row[4] as! [UInt8]))
-    print("longtext", UTF8Encoding.encode(row[5] as! [UInt8]))
-    print("tinyint", row[6] as! Int8)
-    print("utinyint", row[7] as! UInt8)
-    print("smallint", row[8] as! Int16)
-    print("usmallint", row[9] as! UInt16)
-    print("mediumint", row[10] as! Int32)
-    print("umediumint", row[11] as! UInt32)
-    print("int", row[12] as! Int32)
-    print("uint", row[13] as! UInt32)
-    print("bigint", row[14] as! Int64)
-    print("ubigint", row[15] as! UInt64)
-    print("float", row[16] as! Float)
-    print("double", row[17] as! Double)
-    print("decimal", row[18] as! String)
-    print("date", row[19] as! String)
-    print("datetime", row[20] as! String)
-    print("timestamp", row[21] as! String)
-    print("time", row[22] as! String)
-    print("year", row[23] as! String)
-    print("tinyblob", UTF8Encoding.encode(row[24] as! [UInt8]))
-    print("mediumblob", UTF8Encoding.encode(row[25] as! [UInt8]))
-    print("blob", UTF8Encoding.encode(row[26] as! [UInt8]))
-    print("longblob", UTF8Encoding.encode(row[27] as! [UInt8]))
-    print("enum", row[28] as! String)
-    print("set", row[29] as! String)
-    print("bool", row[30] as! Int8)
-    print("binary", row[31] as! String)
-    print("varbinary", row[32] as! String)
-    }
-*/
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
