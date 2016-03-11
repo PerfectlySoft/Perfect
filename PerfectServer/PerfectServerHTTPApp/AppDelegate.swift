7185d841278140e2b8207f3a197eafb7e7c9a32e
Kyle Jessup <badthing@gmail.com>
2016-03-11 16:29:33 -0500

    Licensed under Apache License v2.0
    Former-commit-id: 635e84675be008148bc6cb97433bb91a7488e314

--- PerfectServer/PerfectServerHTTPApp/AppDelegate.swift
+++ PerfectServer/PerfectServerHTTPApp/AppDelegate.swift
@@ -5,22 +5,16 @@
 //  Created by Kyle Jessup on 2015-10-25.
 //	Copyright (C) 2015 PerfectlySoft, Inc.
 //
-//	This program is free software: you can redistribute it and/or modify
-//	it under the terms of the GNU Affero General Public License as
-//	published by the Free Software Foundation, either version 3 of the
-//	License, or (at your option) any later version, as supplemented by the
-//	Perfect Additional Terms.
+//===----------------------------------------------------------------------===//
 //
-//	This program is distributed in the hope that it will be useful,
-//	but WITHOUT ANY WARRANTY; without even the implied warranty of
-//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//	GNU Affero General Public License, as supplemented by the
-//	Perfect Additional Terms, for more details.
+// This source file is part of the Perfect.org open source project
 //
-//	You should have received a copy of the GNU Affero General Public License
-//	and the Perfect Additional Terms that immediately follow the terms and
-//	conditions of the GNU Affero General Public License along with this
-//	program. If not, see <http://www.perfect.org/AGPL_3_0_With_Perfect_Additional_Terms.txt>.
+// Copyright (c) 2015 - 2016 PerfectlySoft Inc. and the Perfect project authors
+// Licensed under Apache License v2.0
+//
+// See http://perfect.org/licensing.html for license information
+//
+//===----------------------------------------------------------------------===//
 //
 
 
