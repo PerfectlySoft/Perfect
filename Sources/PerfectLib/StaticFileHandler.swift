61b9fc87f7db2835036fa202c10f299243a1c14f
Kyle Jessup <badthing@gmail.com>
2016-05-16 22:28:11 -0400

    HTTP range and chunked support. WebConnection writes are async. StaticFileHandler handles huge files
    Former-commit-id: 10f97ad1f3ff6814c6b2b82e9286ec9c8d66bd5f

--- Sources/PerfectLib/StaticFileHandler.swift
+++ Sources/PerfectLib/StaticFileHandler.swift
@@ -19,6 +19,8 @@
 
 public struct StaticFileHandler {
 	
+	let chunkedBufferSize = 1024*200
+	
 	public init() {}
 	
 	public func handleRequest(request: WebRequest, response: WebResponse) {
@@ -38,25 +40,113 @@ public struct StaticFileHandler {
 			return
 		}
 		
-		self.sendFile(response: response, file: file)
-		response.requestCompleted()
+		self.sendFile(request: request, response: response, file: file)
 	}
 	
-	func sendFile(response resp: WebResponse, file: File) {
+	func sendFile(request req: WebRequest, response resp: WebResponse, file: File) {
 		
-		defer {
-			file.close()
+		let contentType = MimeType.forExtension(file.path().pathExtension)
+		let size = file.size()
+		
+		resp.addHeader(name: "Accept-Ranges", value: "bytes")
+		
+		let isHead = req.requestMethod == "HEAD"
+		
+		if let rangeRequest = resp.request.header(named: "Range") {
+			
+			let ranges = self.parseRangeHeader(fromHeader: rangeRequest, max: size)
+			if ranges.count == 1 {
+				let range = ranges[0]
+				let rangeCount = range.count
+				
+				resp.setStatus(code: 206, message: "Partial Content")
+				resp.addHeader(name: "Content-Length", value: "\(rangeCount)")
+				resp.addHeader(name: "Content-Type", value: contentType)
+				resp.addHeader(name: "Content-Range", value: "bytes \(range.lowerBound)-\(range.upperBound-1)/\(size)")
+				
+				if isHead {
+					return resp.requestCompleted()
+				}
+				
+				file.setMarker(to: range.lowerBound)
+				
+				return self.sendFile(remainingBytes: rangeCount, response: resp, file: file) {
+					ok in
+					
+					file.close()
+					resp.requestCompleted()
+				}
+			} else if ranges.count > 0 {
+				
+			}
 		}
 		
-		let size = file.size()
 		resp.setStatus(code: 200, message: "OK")
+		resp.addHeader(name: "Content-Type", value: contentType)
+		resp.addHeader(name: "Content-Length", value: "\(size)")
 		
-		do {
-			let bytes = try file.readSomeBytes(count: size)
-			resp.addHeader(name: "Content-type", value: MimeType.forExtension(file.path().pathExtension))
-			resp.appendBody(bytes: bytes)
-		} catch {
-			resp.setStatus(code: 500, message: "Internal server error")
+		if isHead {
+			return resp.requestCompleted()
+		}
+		
+		self.sendFile(remainingBytes: size, response: resp, file: file) {
+			ok in
+			file.close()
+			resp.requestCompleted()
 		}
 	}
+	
+	func sendFile(remainingBytes remaining: Int, response: WebResponse, file: File, completion: (Bool) -> ()) {
+		
+		let thisRead = min(chunkedBufferSize, remaining)
+		do {
+			let bytes = try file.readSomeBytes(count: thisRead)
+			response.appendBody(bytes: bytes)
+			response.pushBody {
+				ok in
+				
+				if !ok || thisRead == remaining {
+					// done
+					completion(ok)
+				} else {
+					self.sendFile(remainingBytes: remaining - bytes.count, response: response, file: file, completion: completion)
+				}
+			}
+		} catch {
+			completion(false)
+		}
+	}
+	
+	// bytes=0-3/7-9/10-15
+	func parseRangeHeader(fromHeader header: String, max: Int) -> [Range<Int>] {
+		let initialSplit = header.characters.split(separator: "=")
+		guard initialSplit.count == 2 && String(initialSplit[0]) == "bytes" else {
+			return [Range<Int>]()
+		}
+		
+		let ranges = initialSplit[1]
+		return ranges.split(separator: "/").flatMap { self.parseOneRange(fromString: String($0), max: max) }
+	}
+	
+	// 0-3
+	// 0-
+	func parseOneRange(fromString string: String, max: Int) -> Range<Int>? {
+		let split = string.characters.split(separator: "-")
+		
+		if split.count == 1 {
+			guard let lower = Int(String(split[0])) else {
+				return nil
+			}
+			return Range(uncheckedBounds: (lower, max))
+		}
+		
+		guard let lower = Int(String(split[0])), upper = Int(String(split[1])) else {
+			return nil
+		}
+		
+		return Range(uncheckedBounds: (lower, upper+1))
+	}
 }
+
+
+
