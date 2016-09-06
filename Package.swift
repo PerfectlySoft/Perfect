dbc361a59d7e40517219536b5a715e5f820a1cb4
Kyle Jessup <badthing@gmail.com>
2016-09-06 15:30:40 -0400

    Pinned dep versions

--- Package.swift
+++ Package.swift
@@ -30,6 +30,6 @@ urls += ["https://github.com/PerfectlySoft/Perfect-LinuxBridge.git"]
 let package = Package(
 	name: "PerfectLib",
 	targets: [],
-	dependencies: urls.map { .Package(url: $0, versions: Version(0,0,0)..<Version(10,0,0)) },
+	dependencies: urls.map { .Package(url: $0, majorVersion: 2, minor: 0) },
 	exclude: []
 )
