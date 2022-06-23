7a320698104bab95944a08f1a086014b955a343e
Rockford Wei <rocky@perfect.org>
2022-06-23 09:03:12 -0400

    fix project name issue

--- test-macos.sh
+++ test-macos.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-swift package generate-xcodeproj && xcodebuild test -project PerfectClassic.xcodeproj/ -scheme PerfectClassic-Package
+swift package generate-xcodeproj && xcodebuild test -project PerfectLib.xcodeproj/ -scheme PerfectLib-Package
